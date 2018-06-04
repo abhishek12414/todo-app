31e50b878dc7cdf38c014d19b64c383a951aed1c
Abhishek Kumar <abhishek.kumar@mountblue.io>
2018-06-05 00:49:06 +0530

    update js, html and css code

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
-
 let idCount = 0;
 let completedTask = new Array();
 
 const todoSection = document.getElementById('section-todo-list');
 
 function addChild(key) {
-    // console.log(idCount)
-    if(event.key == 'Enter') {
+    if(event.key == 'Enter' && key.value != "") {
         
         //creating new root div
-        let newDiv = document.createElement('div');
-        newDiv.setAttribute("class", "item-list md-checkbox");
-        newDiv.setAttribute("id", `row${++idCount}`);
-        newDiv.setAttribute("draggable", 'true');
-        newDiv.setAttribute("onmousedown", 'dragItems()');
+        let newDiv = document.createElement( 'div' );
+        newDiv.setAttribute( "class", "item-list md-checkbox" );
+        newDiv.setAttribute( "id", `row${++idCount}` );
+        newDiv.setAttribute( "draggable", 'true' );
+        newDiv.setAttribute( "onmousedown", 'dragItems()' );
 
         newDiv.innerHTML = `
             <div> <input type="checkbox" id="checkbox${idCount}" onclick="changeState('row${idCount}')"><label for="checkbox${idCount}">${key.value}</label></div> 
@@ -24,18 +22,18 @@ function addChild(key) {
         updateTaskStatus();
         document.getElementById(key.id).value = "";
     }
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+}
 
 function changeState(divId) {
     
     const div = document.getElementById(`${divId}`);            
     const checkedStatus = div.getElementsByTagName('input')[0].checked;
 
-    if(checkedStatus) {
+    if(checkedStatus)
         div.getElementsByTagName('label')[0].style.textDecoration = 'line-through';
-    } else {
+    else
         div.getElementsByTagName('label')[0].style.textDecoration = 'none';
-    }
+
     updateTaskStatus(divId);
 }
 
@@ -46,14 +44,12 @@ function removeTask(divId) {
         }
     } else {
         removeDiv(divId);
-    }
-    
+    }    
 }
 
 function removeDiv(divId) {
     document.getElementById(divId).remove();
     --idCount;
-    // updateTaskStatus(divId);
     if(completedTask.includes(divId)) {
         const index = completedTask.indexOf(divId);
         completedTask.splice(index, 1);
@@ -80,65 +76,46 @@ function updateTaskStatus(args) {
     }
 }
 
-
-
-
-
-
 // drag item
-
-let dragItem = null;
-var dragSrcEl = null;
+let dragSrcEl = null;
 
 function handleDragStart(e) {
     // Target (this) element is the source node.
     dragSrcEl = this;
-
     e.dataTransfer.effectAllowed = 'move';
     e.dataTransfer.setData('text/html', this.outerHTML);
-
     this.classList.add('dragElem');
-
 }
 
 function handleDragOver(e) {
     if (e.preventDefault) {
-        e.preventDefault(); // Necessary. Allows us to drop.
+        e.preventDefault();
     }
     this.classList.add('over');
 
-    e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+    e.dataTransfer.dropEffect = 'move';
 
     return false;
 }
 
-function handleDragEnter(e) {
-// this / e.target is the current hover target.
-}
+function handleDragEnter(e) {}
 
 function handleDragLeave(e) {
-    this.classList.remove('over');  // this / e.target is previous target element.
+    this.classList.remove('over');
 }
 
 function handleDrop(e) {
-    // this/e.target is current target element.
 
     if (e.stopPropagation) {
-        e.stopPropagation(); // Stops some browsers from redirecting.
+        e.stopPropagation();
     }
 
-    // Don't do anything if dropping the same column we're dragging.
     if (dragSrcEl != this) {
-        // Set the source column's HTML to the HTML of the column we dropped on.
-        //alert(this.outerHTML);
-        //dragSrcEl.innerHTML = this.innerHTML;
-        //this.innerHTML = e.dataTransfer.getData('text/html');
         this.parentNode.removeChild(dragSrcEl);
-        var dropHTML = e.dataTransfer.getData('text/html');
+        let dropHTML = e.dataTransfer.getData('text/html');
         this.insertAdjacentHTML('beforebegin',dropHTML);
-        var dropElem = this.previousSibling;
-        addDnDHandlers(dropElem);
-        
+        let dropElem = this.previousSibling;
+        addDnDHandlers(dropElem);        
     }
     this.classList.remove('over');
     return false;
@@ -147,14 +124,8 @@ function handleDrop(e) {
 function handleDragEnd(e) {
     // this/e.target is the source node.
     this.classList.remove('over');
-
-    /*[].forEach.call(cols, function (col) {
-        col.classList.remove('over');
-    });*/
     updateCheckMark();
 }
-
-let divElem;
 
 function addDnDHandlers(elem) {
     elem.addEventListener('dragstart', handleDragStart, false);
@@ -163,10 +134,9 @@ function addDnDHandlers(elem) {
     elem.addEventListener('dragleave', handleDragLeave, false);
     elem.addEventListener('drop', handleDrop, false);
     elem.addEventListener('dragend', handleDragEnd, false);
-    divElem = elem;
 }
 
 function dragItems() {
-    var cols = document.querySelectorAll('#section-todo-list .item-list');
+    let cols = document.querySelectorAll('#section-todo-list .item-list');
     [].forEach.call(cols, addDnDHandlers);
 }
